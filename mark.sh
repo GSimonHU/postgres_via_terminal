dcaad0a2973ceedb4307a60939e3baeee580f24e
Gabor Simon <simon.gabor87hu@gmail.com>
2021-02-02 11:31:48 +0100

    Setup basic program structure

--- mark.sh
+++ mark.sh
@@ -8,8 +8,22 @@
 #    mark.sh unmark-todo 32
 #
 
+mark_todo(){
+
+}
+
+unmark_todo(){
+    
+}
+
 main() {
-    echo "Your code"
+    if [[ "$1" == "mark-todo" ]]
+    then
+        mark_todo $2
+    elif [[ "$1" == "unmark-todo" ]]
+    then
+        unmark_todo $2
+    fi
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
